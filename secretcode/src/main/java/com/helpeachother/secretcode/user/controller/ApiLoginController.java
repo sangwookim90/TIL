0edbe11705880b0610a25a7f017526df99ab9fa8
Sangwoo Kim <sangwookim90@gmail.com>
2021-06-29 14:13:50 +0900

    log 파일 쓰기

--- secretcode/src/main/java/com/helpeachother/secretcode/user/controller/ApiLoginController.java
+++ secretcode/src/main/java/com/helpeachother/secretcode/user/controller/ApiLoginController.java
@@ -9,6 +9,7 @@ import com.helpeachother.secretcode.user.model.UserLoginToken;
 import com.helpeachother.secretcode.user.service.UserService;
 import com.helpeachother.secretcode.util.JwtUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class ApiLoginController {
@@ -24,10 +26,11 @@ public class ApiLoginController {
     private final UserService userService;
 
     /**
-     * 회원 로그인 히스토리 기능 API
+     * 회원 로그인 히스토리 기능 API + 로그인시 에러가 발생한 경우 로그에 기록하는 기능
      */
     @PostMapping("/api/login")
     public ResponseEntity<?> login(@RequestBody @Valid UserLogin userLogin, Errors errors) {
+
         if(errors.hasErrors()) {
             return ResponseResult.fail("입력값이 정확하지 않습니다.", ResponseError.of(errors.getAllErrors()));
         }
@@ -36,11 +39,13 @@ public class ApiLoginController {
         try {
             user = userService.login(userLogin);
         } catch (BizException e) {
+            log.info("로그인 에러:" + e.getMessage());
             return ResponseResult.fail(e.getMessage());
         }
 
         UserLoginToken userLoginToken = JwtUtils.createToken(user);
         if(userLoginToken == null) {
+            log.info("JWT 생성 에러");
             return ResponseResult.fail("JWT 생성에 실패하였습니다.");
         }
         return ResponseResult.success(userLoginToken);
