e849f15a892bc8472c6e3b545a527156daee4b4c
Sangwoo Kim <sangwookim90@gmail.com>
2021-07-18 15:31:19 +0900

    공공데이터 openapi client

--- secretcode/src/main/java/com/helpeachother/secretcode/extra/controller/ApiExtraController.java
+++ secretcode/src/main/java/com/helpeachother/secretcode/extra/controller/ApiExtraController.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.helpeachother.secretcode.common.model.ResponseResult;
 import com.helpeachother.secretcode.extra.model.OpenApiResult;
+import com.helpeachother.secretcode.extra.model.PharmacySearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -23,12 +25,14 @@ public class ApiExtraController {
      * RestTemplate을 사용하여 공공데이터포털의 공공API를 연동하여 약국목록 가져오는 API
      */
     @GetMapping("/api/extra/pharmacy")
-    public ResponseEntity<?> pharmacy() {
-        String url = "http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyListInfoInqire?serviceKey=%s";
+    public ResponseEntity<?> pharmacy(@RequestBody PharmacySearch pharmacySearch) {
         String apiKey = "iGS4ZjM37Q7ZF276W6MOVsmxQ6cW1k2j5hIKaxNGFrLz6Jk9NL7bwPdkzskhkcA%2F2oG394OFeWlYX4nnkyOZuQ%3D%3D&";
+        String url = String.format("http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyListInfoInqire?serviceKey=%s", apiKey);
 
         String apiResult = "";
         try {
+            url += String.format("");
+
             URI uri = new URI(String.format(url, apiKey));
             RestTemplate restTemplate = new RestTemplate();
             HttpHeaders headers = new HttpHeaders();
