143ddfac705525e12dfcb4bfc1ef2d4fba0ffe47
Sangwoo Kim <sangwookim90@gmail.com>
2021-08-06 21:32:09 +0900

    feat: Algorithm - template-FastReader 추가

--- algorithm/src/main/java/template.java
+++ algorithm/src/main/java/template.java
@@ -1,4 +1,6 @@
+import java.io.*;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class template {
 
@@ -10,5 +12,52 @@ public class template {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+
+        long nextLong() { return Long.parseLong(next()); }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
     }
 }
