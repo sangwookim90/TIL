5a74bc12204ecfec8bd470fccf17b21a861f6582
Sangwoo Kim <sangwookim90@gmail.com>
2021-08-07 09:56:55 +0900

    feat: Algorithm - template 수정

--- algorithm/src/main/java/template.java
+++ algorithm/src/main/java/template.java
@@ -1,16 +1,16 @@
 import java.io.*;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class template {
 
-    static void solution() {
+    static StringBuilder sb = new StringBuilder();
 
-
+    static void input() {
+        FastReader scan = new FastReader();
     }
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        input();
 
     }
 
