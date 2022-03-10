460ef126255498b925badaaa73cb792a5622a665
Sangwoo Kim <sangwookim90@gmail.com>
2022-03-10 14:53:57 +0900

    feat: gsap

---
+++ frontend-study/concept/BEM.md
@@ -0,0 +1,6 @@
+# BEM (Block Elemenet Modifier)
+
+> HTML 클래스 속성의 작명법
+
+- 요소\_\_일부분: Underscore(Lodash) 기호로 요소의 일부분을 표시
+- 요소--상태: Hyphen(Dash) 기호로 요소의 상태를 표시
--- frontend-study/starbucks/js/main.js
+++ frontend-study/starbucks/js/main.js
@@ -14,3 +14,27 @@ searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 })
+
+// 일정량의 스크롤다운 시 배지 삭제
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function() {
+  console.log('scroll');
+  if(window.scrollY > 500) {
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, 0.6, {
+      opacity: 0,
+      display: 'none'
+    });
+
+    // badgeEl.style.display = 'none';
+  } else {
+    // badgeEl.style.display = 'block';
+    gsap.to(badgeEl, 0.6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
+
+// _.throttle(함수, 시간 (ms)) , d
