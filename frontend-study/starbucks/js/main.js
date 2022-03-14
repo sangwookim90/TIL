af391829bb8cfcb4c751e571fe0caeebf835b507
Sangwoo Kim <sangwookim90@gmail.com>
2022-03-14 14:25:02 +0900

    feat: 순차적으로 요소 보이기

--- frontend-study/starbucks/js/main.js
+++ frontend-study/starbucks/js/main.js
@@ -36,5 +36,13 @@ window.addEventListener('scroll', _.throttle(function() {
     });
   }
 }, 300));
-
 // _.throttle(함수, 시간 (ms)) , d
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  // gsap.to(요소, 지속시간, 옵션);
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  });
+});
