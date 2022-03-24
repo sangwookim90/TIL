89bcf49b080c6089c334e2abd87c640ccccb9c1c
Sangwoo Kim <sangwookim90@gmail.com>
2022-03-24 14:55:14 +0900

    feat: Top button

--- frontend-study/starbucks/js/main.js
+++ frontend-study/starbucks/js/main.js
@@ -17,6 +17,7 @@ searchInputEl.addEventListener('blur', function () {
 
 // 일정량의 스크롤다운 시 배지 삭제
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function() {
   if(window.scrollY > 500) {
@@ -24,6 +25,10 @@ window.addEventListener('scroll', _.throttle(function() {
     gsap.to(badgeEl, 0.6, {
       opacity: 0,
       display: 'none'
+    });
+    // 버튼 보이기
+    gsap.to(toTopEl, .2, {
+      x: 0
     });
 
     // badgeEl.style.display = 'none';
@@ -33,9 +38,21 @@ window.addEventListener('scroll', _.throttle(function() {
       opacity: 1,
       display: 'block'
     });
+
+    // 버튼 숨기기
+    gsap.to(toTopEl, .2, {
+      x: 100
+    });
   }
 }, 300));
 // _.throttle(함수, 시간 (ms)) , d
+
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, .7, {
+    scrollTo: 0 // ScrollToPlugin 필요
+  });
+});
+
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function (fadeEl, index) {
